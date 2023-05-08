2e69f58c70bee4f7c8c1c13cb93640f77abf0199
Daniel Curtin <dcurtin.work@gmail.com>
2023-05-08 17:36:44 -0600

    Add type buttons and search button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,6 +10,17 @@ const App = () => {
       <h1>Searchix</h1>
       <Search></Search>
       <Saved></Saved>
+      <section className='type-select'>
+        <label className='type-label'>
+          <input type='radio' value='Prefix' name='fix-selection' className='type-btn'></input>
+          Prefix
+        </label>
+        <label className='type-label'>
+          <input type='radio' value='Suffix' name='fix-selection' className='type-btn'></input>
+          Suffix
+        </label>
+      </section>
+      <button type='submit' className='search-btn'>Search</button>
     </main>
   );
 };
